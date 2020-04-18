7e00c963feba34e927be5054f8519efc1a4c2d9f
dipolo12q <57011274+dipolo12q@users.noreply.github.com>
2020-04-17 23:19:31 -0500

    PointersFunc

---
+++ main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+void * _bubblesort_(
+        void * _array_,
+        void (*_puntero_) (void *, int _i, int _k),
+        int _tam_
+){
+    int _k = 0;
+    for (int i = 0; i < _tam_ ; i ++)
+        for(int k = 0; k < _tam_-1; k++)
+            _puntero_(_array_,i,k);
+    return _array_;
+}
+
+void  _b_int_ (void * _vector_, int _i,int _k){
+    int* _v = (int*)_vector_;
+    if(_v[_i] < _v[_k]) {
+        auto temp = _v[_k];
+        _v[_k] = _v[_i];
+        _v[_i] = temp;
+    }
+}
+
+void  _b_float_ (void * _vector_, int _i,int _k){
+    float* _v = (float*)_vector_;
+    if(_v[_i] < _v[_k]){
+        auto temp = _v[_k];
+        _v[_k] = _v[_i];
+        _v[_i] = temp;
+    }
+}
+
+void  _b_double_ (void * _vector_, int _i,int _k){
+    double* _v = (double*)_vector_;
+    if(_v[_i] < _v[_k]){
+        auto temp = _v[_k];
+        _v[_k] = _v[_i];
+        _v[_i] = temp;
+    }
+}
+
+void  _b_char_ (void * _vector_, int _i,int _k){
+    char* _v = (char*)_vector_;
+    if(_v[_i] < _v[_k]){
+        auto temp = _v[_k];
+        _v[_k] = _v[_i];
+        _v[_i] = temp;
+    }
+}
+
+int main(){
+    int i = 0;
+    int _array_1 [5] = {2,20,7,108,9};
+    float _array_2 [5] = {4.4,5.4,9.1,7.4,2.2};
+    double _array_3 [5] = {3.0,8.0,2.0,4.0,15.0};
+    char _array_4 [6] = {'k','a','r','l','o','s'};
+    _bubblesort_(_array_1,_b_int_,5);
+    for(auto i : _array_1)
+        cout << i << " ";
+    cout<<endl;
+    _bubblesort_(_array_2,_b_float_,5);
+    for(auto i : _array_2)
+        cout << i << " ";
+    cout<<endl;
+    _bubblesort_(_array_3,_b_double_,5);
+    for(auto i : _array_3)
+        cout << i << " ";
+    cout<<endl;
+    _bubblesort_(_array_4,_b_char_,6);
+    for(auto i : _array_4)
+        cout << i << " ";
+    cout<<endl;
+    return 0;
+}
